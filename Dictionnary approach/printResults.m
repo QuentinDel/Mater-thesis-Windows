94d238f96c224329073897611d2bce0c8d3cfa1a
QuentinDel <quentindelooz@gmail.com>
2018-01-29 11:15:34 +0100

    PrintResults

---
+++ Dictionnary approach/printResults.m
@@ -0,0 +1,11 @@
+for i = 1 : size(results, 1)
+   figure;
+   hold on
+   title(strcat('Dataset:', num2str(i)));
+   for j = 1 : size(results,3)
+      plot(size_windows,results(i, :, j), '-'); 
+   end
+   hold off
+   lgd = legend('5', '10', '15');
+   title(lgd, 'nb neighbors');
+end
